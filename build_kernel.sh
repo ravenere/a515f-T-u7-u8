92ad3b4390210a34920052915b542115ef73bad7
physwizz <pollockg86@gmail.com>
2025-03-25 12:48:16 +1100

    toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/grahame/toolchains/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CC= /home/grahame/a515f-T-u7/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 
